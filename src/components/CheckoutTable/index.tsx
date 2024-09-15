b964e65d9a05ab47aea2b7dff28a11386e4a61d8
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-09-15 18:20:43 -0400

    feat(components): added initial checkout table ui

---
+++ src/components/CheckoutTable/index.tsx
@@ -0,0 +1,53 @@
+import Icon from '#/assets/svg/cube-solid.svg?react';
+import styles from './styles.module.css';
+
+type BasicInvoice = {
+  id: string,
+  clientName: string,
+  description: string,
+  type: 'poduct' | 'service' | 'voucher',
+  bookingDate: string,
+  purchaseTime: string,
+  price: number,
+  status: 'draft' | 'updaid' | 'paid',
+}
+
+export function CheckoutTable () {
+
+  const tableRows = Array(20).fill(0).map((_, index) => (
+    <tr key={index}>
+      <td scope="row">{index}</td>
+      <td>Tony Alarez</td>
+      <td>Express Food</td>
+      <td>Service</td>
+      <td>11/08/22/ 13:00 - 13:30</td>
+      <td>-</td>
+      <td>25.000</td>
+      <td>Draft</td>
+      <td className={styles.actions}>
+        <button className={styles.button}>
+          <Icon height={30} />
+        </button>
+      </td>
+    </tr>
+  ))
+
+  return (
+    <table className={styles.CheckoutTable}>
+      <thead>
+        <tr>
+          <th scope="col">Ref<br />#</th>
+          <th scope="col">Client</th>
+          <th scope="col">Description</th>
+          <th scope="col">Type</th>
+          <th scope="col">Booking Date</th>
+          <th scope="col">Purchase Time</th>
+          <th scope="col">Price</th>
+          <th scope="col">Status</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>{tableRows}</tbody>
+    </table>
+  )
+}
