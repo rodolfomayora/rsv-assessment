5e6d1b12bc67319443082b3fef573bb0d3a06c25
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-09-16 06:32:03 -0400

    feat(components): replaced icons on navigation bar

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,4 +1,12 @@
-import Icon from '#/assets/svg/cube-solid.svg?react';
+import CalendarIcon from '#/assets/svg/calendar-regular.svg?react';
+import CheckoutIcon from '#/assets/svg/money-bills-solid.svg?react';
+import ClientsIcon from '#/assets/svg/address-book-regular.svg?react';
+import ProductIcon from '#/assets/svg/products-solid.svg?react';
+import ServicesIcon from '#/assets/svg/scissors-solid.svg?react';
+import FormsIcon from '#/assets/svg/paste-regular.svg?react';
+import MarketingIcon from '#/assets/svg/bullhorn-solid.svg?react';
+import ReportIcon from '#/assets/svg/square-poll-vertical-solid.svg?react';
+import SettingIcon from '#/assets/svg/gear-solid.svg?react';
 import { NavigationItem } from './NavigationItem';
 import styles from './styles.module.css';
 
@@ -6,18 +14,53 @@ export function Navigation () {
   return (
     <nav className={styles.Navigation}>
       <ul className={styles.list}>
-        <li><a><NavigationItem >Calendar</NavigationItem></a></li>
-        <li><a><NavigationItem icon={<Icon width={30} />}>Checkout</NavigationItem></a></li>
+        <li><a>
+          <NavigationItem icon={<CalendarIcon height={24} />}>
+            Calendar
+          </NavigationItem>
+        </a></li>
+        <li><a>
+          <NavigationItem icon={<CheckoutIcon width={30} />}>
+            Checkout
+          </NavigationItem>
+        </a></li>
         <div className={styles.separator} />
-        <li><a><NavigationItem>Charts</NavigationItem></a></li>
-        <li><a><NavigationItem>Proucts</NavigationItem></a></li>
-        <li><a><NavigationItem>Services</NavigationItem></a></li>
-        <li><a><NavigationItem>Forms</NavigationItem></a></li>
-        <li><a><NavigationItem>Marketing</NavigationItem></a></li>
-        <li><a><NavigationItem>Forms</NavigationItem></a></li>
+        <li><a>
+          <NavigationItem icon={<ClientsIcon width={24} />}>
+            Clients
+          </NavigationItem>
+        </a></li>
+        <li><a>
+          <NavigationItem icon={<ProductIcon width={24} />}>
+            Proucts
+          </NavigationItem>
+        </a></li>
+        <li><a>
+          <NavigationItem icon={<ServicesIcon width={24} />}>
+            Services
+          </NavigationItem>
+        </a></li>
+        <li><a>
+          <NavigationItem icon={<FormsIcon width={24} />}>
+            Forms
+          </NavigationItem>
+        </a></li>
         <div className={styles.separator} />
-        <li><a><NavigationItem>Reports</NavigationItem></a></li>
-        <li><a><NavigationItem>Settings</NavigationItem></a></li>
+        <li><a>
+          <NavigationItem icon={<MarketingIcon width={24} />}>
+            Marketing
+          </NavigationItem>
+        </a></li>
+        <li><a>
+          <NavigationItem icon={<ReportIcon width={24} />}>
+            Reports
+          </NavigationItem>
+        </a></li>
+        <li><a>
+          <NavigationItem icon={<SettingIcon width={24} />}>
+            Settings
+          </NavigationItem>
+        </a></li>
       </ul>
     </nav>
   )
