98628fa6bc7869d8af67524e17378edc7ce94c22
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-09-16 16:12:41 -0400

    feat(config): define app basic routes

---
+++ src/config/routes.ts
@@ -0,0 +1,12 @@
+export const routes = {
+  root: '/',
+  checkout:'/checkout',
+  calendar:'/calendar',
+  clients:'/clients',
+  forms:'/forms',
+  marketing:'/marketing',
+  products:'/products',
+  reports:'/reports',
+  services:'/services',
+  settings:'/settings',
+} as const;
