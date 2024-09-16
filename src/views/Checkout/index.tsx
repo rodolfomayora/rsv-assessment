f23d27836ace20c22fc78b6412032f840d819abc
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-09-16 12:17:06 -0400

    feat(views): implemented checkout summary

--- src/views/Checkout/index.tsx
+++ src/views/Checkout/index.tsx
@@ -1,14 +1,19 @@
 import { Dashboard } from '#/layouts/Dashboard';
 import { CheckoutTable } from './CheckoutTable';
 import { CheckoutFilters } from './CheckoutFilters';
+import { CheckoutSummary } from './CheckoutSummary';
+
 import styles from './styles.module.css';
 
 export function Checkout () {
   return (
     <Dashboard>
       <main className={styles.Checkout}>
-        <CheckoutFilters />
-        <CheckoutTable />
+        <div className={styles.table}>
+          <CheckoutFilters />
+          <CheckoutTable />
+        </div>
+        <CheckoutSummary />
       </main>
     </Dashboard>
   )
