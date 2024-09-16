cc792c8f7b4653b63a9e68da04578e381255f39c
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-09-16 06:31:19 -0400

    feat(components): replaced icons on header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,13 @@
-import { useState } from 'react';
 import Logo from '#/assets/svg/reservi-logo.svg?react';
-import Icon from '#/assets/svg/cube-solid.svg?react';
+import WhatsappIcon from '#/assets/svg/whatsapp-brands-solid.svg?react';
+import EmailIcon from '#/assets/svg/envelope-regular.svg?react';
+import MobileIcon from '#/assets/svg/mobile-screen-solid.svg?react';
+import SearchIcon from '#/assets/svg/search-solid.svg?react';
+import WalletIcon from '#/assets/svg/wallet-solid.svg?react';
+import BellIcon from '#/assets/svg/bell-regular.svg?react';
+import CloudIcon from '#/assets/svg/cloud-solid.svg?react';
+import { UserPicture } from './UserPicture';
 import styles from './styles.module.css';
-
-type Props = {
-  src: string,
-}
-
-function UserPicture ({ src = "" }: Props) {
-  const [error, setError] = useState(false);
-  if (error) return <div className={styles.picture}>JM</div>; // placeholder
-  return (
-    <img className={styles.picture}
-      src={src}
-      alt="User Profile Picture"
-      loading="eager"
-      decoding="sync"
-      width={50}
-      height={50}
-      onError={() => setError(true)}
-    />
-  )
-}
 
 export function Header () {
   return (
@@ -29,27 +15,31 @@ export function Header () {
       <Logo className={styles.logo} />
       <h1 className={styles.title}>Checkout</h1>
       <div className={styles.wrapper}>
-        <Icon height={20} />
+        <CloudIcon height={20} fill="#adaeb2" />
         <div className={styles.messages}>
           <div className={styles.platform}>
-            <Icon height={20} />
+            <EmailIcon width={18} />
             <span>1000</span>
           </div>
           <div className={styles.platform}>
-            <Icon height={20} />
+            <MobileIcon height={18} />
             <span>400</span>
           </div>
           <div className={styles.platform}>
-            <Icon height={20} />
+            <WhatsappIcon height={18} />
             <span>200</span>
           </div>
         </div>
         <button className={styles.wallet}>
-          <Icon height={20} />
+          <WalletIcon height={20} />
           <span>wallet</span>
         </button>
-        <Icon height={30} />
-        <Icon height={30} />
+        <button className={styles.search}>
+          <SearchIcon height={30} />
+        </button>
+        <button className={styles.notifications}>
+          <BellIcon height={30} />
+        </button>
         {/* <UserPicture src="https://flagcdn.com/ch.svg" /> */}
         <UserPicture src="" />
       </div>
