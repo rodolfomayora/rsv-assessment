6057a9bc5d513d062530683a65703109637342b6
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-09-16 16:27:27 -0400

    feat(root): implemented dynamic tab browser

--- src/layouts/Dashboard/index.tsx
+++ src/layouts/Dashboard/index.tsx
@@ -1,16 +1,19 @@
 import type { ReactNode } from 'react';
 import { Header } from '#/components/Header';
 import { Navigation } from '#/components/Navigation';
+import { useTabTitle } from '#/hooks/useTabTitle';
 import styles from './styles.module.css';
 
 type Props = {
   children: ReactNode,
+  pageTitle: string,
 }
 
-export function Dashboard ({ children }: Props) {
+export function Dashboard ({ children, pageTitle }: Props) {
+  useTabTitle(pageTitle);
   return (
     <>
-      <Header />
+      <Header pageTitle={pageTitle} />
       <div className={styles.Layout}>
         <Navigation />
         {children}
--- src/views/Checkout/index.tsx
+++ src/views/Checkout/index.tsx
@@ -2,12 +2,11 @@ import { Dashboard } from '#/layouts/Dashboard';
 import { CheckoutTable } from './CheckoutTable';
 import { CheckoutFilters } from './CheckoutFilters';
 import { CheckoutSummary } from './CheckoutSummary';
-
 import styles from './styles.module.css';
 
 export function Checkout () {
   return (
-    <Dashboard>
+    <Dashboard pageTitle="Checkout">
       <main className={styles.Checkout}>
         <div className={styles.table}>
           <CheckoutFilters />
