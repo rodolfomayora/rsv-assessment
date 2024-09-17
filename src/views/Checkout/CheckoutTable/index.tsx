3f035add910c5d475ec4ce2c2e4cd04566284540
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-09-17 06:35:20 -0400

    feat(views): configured checkout table to handle api calls

--- src/views/Checkout/CheckoutTable/index.tsx
+++ src/views/Checkout/CheckoutTable/index.tsx
@@ -1,41 +1,55 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { InvoicesApi } from '#/api/dummy/invoices';
+import { BasicInvoice } from '#/types/BasicInvoice';
 import EditIcon from '#/assets/svg/pen-solid.svg?react';
-// import BillIcon from '#/assets/';
+import BillIcon from '#/assets/svg/receipt-solid.svg?react';
+import { NoResults } from './NoResults';
 import styles from './styles.module.css';
 
-// type BasicInvoice = {
-//   id: string,
-//   clientName: string,
-//   description: string,
-//   type: 'poduct' | 'service' | 'voucher',
-//   bookingDate: string,
-//   purchaseTime: string,
-//   price: number,
-//   status: 'draft' | 'updaid' | 'paid',
-// }
-
+// export function CheckoutTable ({ limit, offset }: Props) {
 export function CheckoutTable () {
+  const { data } = useSuspenseQuery<BasicInvoice[]>({
+    queryKey: ['bills'],
+    queryFn: () => InvoicesApi.getAll(),
+    // queryFn: async () => {
+    //   const result = await InvoicesApi.getAll();
+    //   // throw new Error();
+    //   // return [];
+    //   return result;
+    // }
+  })
 
-  const tableRows = Array(20).fill(0).map((_, index) => (
-    <tr key={index}>
-      <td scope="row">{index}</td>
-      <td>Tony Alarez</td>
-      <td>Express Food</td>
-      <td>Service</td>
-      <td>11/08/22/ 13:00 - 13:30</td>
-      <td>-</td>
-      <td>25.000</td>
-      <td>Draft</td>
-      <td className={styles.actions}>
-        <button className={styles.button}>
-          <EditIcon height={24} />
-        </button>
-      </td>
-    </tr>
-  ))
+  if (!data || data.length === 0) return <NoResults />;
+
+  const tableRows = data.map((bill) => {
+    const isPaid = bill.status === 'paid';
+    const icon = isPaid
+      ? <BillIcon height={24} />
+      : <EditIcon height={24} />
+      ;
+
+    return (
+      <tr key={bill.id}>
+        <td scope="row">{bill.id}</td>
+        <td>{bill.clientName}</td>
+        <td>{bill.description}</td>
+        <td>{bill.type}</td>
+        <td>{bill.bookingDate}</td>
+        <td>{bill.purchaseTime}</td>
+        <td>{bill.price}</td>
+        <td>{bill.status}</td>
+        <td className={styles.actions}>
+          <button className={styles.button}>
+            {icon} 
+          </button>
+        </td>
+      </tr>
+    )
+  })
 
   return (
     <table className={styles.CheckoutTable}>
-      <thead>
+      <thead className={styles.thead}>
         <tr>
           <th scope="col">Ref<br />#</th>
           <th scope="col">Client</th>
@@ -48,7 +62,7 @@ export function CheckoutTable () {
           <th scope="col"></th>
         </tr>
       </thead>
-      <tbody>{tableRows}</tbody>
+      <tbody className={styles.tbody}>{tableRows}</tbody>
     </table>
   )
 }
--- src/views/Checkout/index.tsx
+++ src/views/Checkout/index.tsx
@@ -1,5 +1,7 @@
+import { Suspense } from 'react';
 import { Dashboard } from '#/layouts/Dashboard';
 import { CheckoutTable } from './CheckoutTable';
+import { SuspenseCheckoutTable } from './SuspenseCheckoutTable';
 import { CheckoutFilters } from './CheckoutFilters';
 import { CheckoutSummary } from './CheckoutSummary';
 import styles from './styles.module.css';
@@ -10,7 +12,9 @@ export function Checkout () {
       <main className={styles.Checkout}>
         <div className={styles.table}>
           <CheckoutFilters />
-          <CheckoutTable />
+          <Suspense fallback={<SuspenseCheckoutTable />}>
+            <CheckoutTable />
+          </Suspense>
         </div>
         <CheckoutSummary />
       </main>
